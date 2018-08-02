184a78fc4064074c51aa3f597283455398956ce7
Daniel Somoano <dnsomoano@gmail.com>
2018-08-01 22:52:48 -0400

    Finished js, now time to spruce up content

--- public/main.js
+++ public/main.js
@@ -1,45 +1,78 @@
 // const main = () => {
 //   document.querySelector('h1').textContent += '?'
 // }
-
+"use strict";
 // document.addEventListener('DOMContentLoaded', main)
 // Starting arrays
-const suits = [ 'clubs', 'diamonds', 'hearts', 'spades' ];
-const ranks = [ 'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King' ];
+const suits = ["clubs", "diamonds", "hearts", "spades"];
+const ranks = [
+  "Ace",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "Jack",
+  "Queen",
+  "King"
+];
 
 let deck = [];
-const createDeck = () => {
-	//   number totalCards = 52;
-	for (i = 0; i < ranks.length; i++) {
-		for (j = 0; j < suits.length; j++) {
-			cards = ranks[i] + ' of ' + suits[j];
-			deck.push(cards);
-			console.log(cards);
-		}
-	}
-	console.log(deck.length);
+
+// Functions are daisy-chained together
+const createDeckAndShuffle = () => {
+  // First loop = 13 * Second loop 4 = 52 loops
+  // First loop
+  deck = [];
+  for (let i = 0; i < ranks.length; i++) {
+    // second loop
+    for (let j = 0; j < suits.length; j++) {
+      // Bucket for temporary values
+      deck.push(ranks[i] + " of " + suits[j]);
+      // Call list of cards
+      //   console.log(cards);
+    }
+  }
+  // Call length of array to confirm 52 cards
+  //   console.log(cards.length);
+  shuffle();
 };
 
+// Fisher-Yates Shuffle
 let shuffle = () => {
-	for (i = 0; i < deck.length; i++) {
-		let j = Math.floor(Math.random() * i);
-		temp = deck[j];
-		deck[j] = deck[i];
-		deck[i] = temp;
-	}
-	let hand = deck.pop()
-	console.log(hand);
+  // For loop will loop 52x
+  for (let i = 0; i < deck.length; i++) {
+    const j = Math.floor(Math.random() * i);
+    // temporary bucket for array deck
+    const temp = deck[j];
+    // value which dumped into temporary is overwritten
+    deck[j] = deck[i];
+    // temporary bucket overwrites the previous array that overwrote
+    deck[i] = temp;
+  }
+  // pop method removes the last element in the array deck
+  resultCard();
 };
-// let shuffle = () => {
-// 	for (i = 0; i < deck.length; i++) {
-// 		let j = Math.floor(Math.random() * i);
-// 		temp = deck[j];
-// 		deck[j] = deck[i];
-// 		deck[i] = deck[j];
-// 	}
-// 	console.log(deck[j]);
-// };
+
+const resultCard = () => {
+  let hand = deck.pop();
+  // debug message for pop value
+  console.log(hand);
+  // Create new line element for result
+  const newLi = document.createElement("li");
+  // New line writes the last element named hand
+  newLi.textContent = hand;
+  // Calls new line back to the DOM
+  document.querySelector("#random-result").appendChild(newLi);
+};
 
 // Event listeners
-document.querySelector('.create-deck-button').addEventListener('click', createDeck);
-document.querySelector('.shuffle-button').addEventListener('click', shuffle);
+document.querySelector(".shuffle-button").addEventListener("click", shuffle);
+// const randomResultSelector = document.querySelector('.random-result').value
+// Event listener for testing shuffle
+// document.querySelector(".shuffle-button").addEventListener("click", shuffle);
+createDeckAndShuffle();
